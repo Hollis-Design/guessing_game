554a176199cb86ffa79f95b1dd1235d0bbd291e8
Hollis-Design <hollisc@byui.edu>
2024-07-08 13:46:50 -0600

    Fixed syntax errors

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    loop {
-        println!("Guess the number!");
-        let secret_number= rand::thread_rng().gen_range(1..=100);
-        println!("The secret # genereated is: {secret_number}");
     
+    println!("Guess the number!");
+    let secret_number= rand::thread_rng().gen_range(1..=100);
+    println!("The secret # genereated is: {secret_number}");
+    
+    loop {
+           
         println!("Please input your guess. ");
 
         //Input must be read in as a string
@@ -18,19 +20,20 @@ fn main() {
             .expect("Failed to read line");
 
         //Convert string input to an unsigned 32 bit #
-        let guess: u32 = guess.trim().parse() {
+        let guess:u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-        }
+        };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => 
+            Ordering::Equal => {
                 println!("You win!");
                 break;
+            }
         }
     }
 }
